be4fc43a7fa1307cc0a655a0c4e9b5ff24eb0436
Jerry Wang <focaaby@gmail.com>
2017-09-01 15:53:39 +0800

    Fixed some tool to replace original name

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Install homebrew
 if type brew &> /dev/null; then
@@ -16,33 +16,33 @@ brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+brew install coreutils --with-default-names
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+brew install findutils --with-default-names
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`. To do so, run `sudo chsh -s /usr/local/bin/bash`.
-brew install bash
+# brew install bash
 brew tap homebrew/versions
-brew install bash-completion2
+# brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-	echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-	chsh -s /usr/local/bin/bash;
-fi;
+# if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+# 	echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+# 	chsh -s /usr/local/bin/bash;
+# fi;
 
 brew tap caskroom/fonts
 brew update
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
+brew install homebrew/dupes/grep --with-default-names
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 
@@ -60,3 +60,4 @@ brew install node yarn nmap mtr tree wget git
 
 # Install zsh
 brew install zsh
+&& chsh -s /bin/zsh
